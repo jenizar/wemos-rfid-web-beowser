01848a63a4e46b0af6fab1d3d79c4a66e2dc0975
John Eswin Nizar <jenizar@users.noreply.github.com>
2023-04-24 21:33:18 +0700

    table sql
    table sql

---
+++ table/rfidtag.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2023 at 04:22 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `websensor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rfidtag`
+--
+
+CREATE TABLE `rfidtag` (
+  `tagid` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rfidtag`
+--
+
+INSERT INTO `rfidtag` (`tagid`) VALUES
+('51076C1A');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rfidtag`
+--
+ALTER TABLE `rfidtag`
+  ADD PRIMARY KEY (`tagid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
